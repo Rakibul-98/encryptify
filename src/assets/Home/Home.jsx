12ed8fe1753a2695eb426a05700002811208bc04
Rakibul-98 <rakibul.rupom2001@gmail.com>
2024-03-30 18:02:43 +0600

    icon and bg image added

--- src/assets/Home/Home.jsx
+++ src/assets/Home/Home.jsx
@@ -1,8 +1,16 @@
+import SearchBar from "./components/SearchBar";
 
 export default function Home() {
-  return (
-    <div>
-        <h1>Welcome to Encryptify</h1>
-    </div>
-  )
+    return (
+        <div className="relative">
+            <div className="relative">
+                <div className="absolute inset-0 w-full h-screen bg-sky-300 bg-opacity-50"></div>
+                <img className="h-screen" src="https://i.ibb.co/dj5T6Hm/encryptify-banner.jpg" alt="bg-image" />
+            </div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h1 className="text-6xl font-bold mb-10"><span className="text-white bg-red-500 pb-1 pr-1">Encrypt</span><span className="text-red-500 bg-white pb-1">ify</span></h1>
+                <SearchBar/>
+            </div>
+        </div>
+    )
 }
