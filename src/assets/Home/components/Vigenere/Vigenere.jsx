5af58c915727ab501b8c6f13fa3c991b7688729e
Rakibul-98 <rakibul.rupom2001@gmail.com>
2024-04-30 19:24:53 +0600

    vigenere done

--- src/assets/Home/components/Vigenere/Vigenere.jsx
+++ src/assets/Home/components/Vigenere/Vigenere.jsx
@@ -8,21 +8,44 @@ export default function Vigenere() {
   const [vigenereData, setVigenereData] = useState([]);
 
   let { plainText, key } = vigenereData;
-  let resultText ='';
+  let resultText = '';
 
-  const generateCipherText = (plainText, key) => {
-    console.log("Cipher text",plainText,key);
-  }
+  const generateCipherText = (plaintext, key) => {
+    plaintext = plaintext.replace(/\s/g, '');
+    key = key.replace(/\s/g, '');
 
-  const generatePlainText = (plainText, key) => {
-    console.log("Plain text",plainText,key);
+    const repeatedKey = key.repeat(Math.ceil(plaintext.length / key.length)).toUpperCase();
+
+    const encryptedChars = plaintext.split('').map((char, index) => {
+        const plaintextCharCode = char.charCodeAt(0);
+        const keyCharCode = repeatedKey.charCodeAt(index);
+        const encryptedCharCode = (plaintextCharCode - 65 + keyCharCode - 65) % 26 + 65;
+        return String.fromCharCode(encryptedCharCode);
+    });
+    return encryptedChars.join('');
+}
+
+
+  const generatePlainText = (cipherText, key) => {
+    cipherText = cipherText.replace(/\s/g, '');
+    key = key.replace(/\s/g, '');
+
+    const repeatedKey = key.repeat(Math.ceil(cipherText.length / key.length)).toUpperCase();
+
+    const decryptedChars = cipherText.split('').map((char, index) => {
+        const cipherTextCharCode = char.charCodeAt(0);
+        const keyCharCode = repeatedKey.charCodeAt(index);
+        const decryptedCharCode = (cipherTextCharCode - keyCharCode + 26) % 26 + 65;
+        return String.fromCharCode(decryptedCharCode);
+    });
+    return decryptedChars.join('');
   }
 
   if (vigenereData.optn?.toLowerCase() === 'encrypt') {
-    resultText = generateCipherText(plainText, parseInt(key));
+    resultText = generateCipherText(plainText.toUpperCase(), key.toUpperCase());
   }
   else if (vigenereData.optn?.toLowerCase() === 'decrypt') {
-    resultText = generatePlainText(plainText, parseInt(key));
+    resultText = generatePlainText(plainText.toUpperCase(), key.toUpperCase());
   }
 
   return (
